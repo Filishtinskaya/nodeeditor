3aac0a86a9d4eabec875c8819a47d831534df8f8
Tetiana Filishtynska <filishtinskaja@gmail.com>
2021-05-22 15:06:17 +0300

    removing actOptions

--- examples/game/NodeForm.cpp
+++ examples/game/NodeForm.cpp
@@ -113,6 +113,20 @@ QGroupBox* NodeForm::addPlaceForActOption(ActOption &actOption)
     for (auto& param : actOption.parameterValues) {
         layout->addRow(param->schema.name, makeEditingWidget(param.get()));
     }
+    auto btn = new QPushButton(box);
+    btn->setText("Delete");
+    layout->addWidget(btn);
+    connect(btn, &QPushButton::pressed, [form = this, actOption = &actOption, box](){
+        box->setParent(NULL);
+        delete box;
+        auto& vec = form->data.actOptions;
+        for (auto it = vec.begin(); it != vec.end(); it++) {
+            if (&(*it) == actOption) {
+                vec.erase(it);
+                break;
+            }
+        }
+    });
     return box;
 }
 
@@ -136,14 +150,9 @@ NodeForm::NodeForm(NodeData& aData) :
 \
     auto toolBar = new QToolBar();
     ui->formLayout_2->addWidget(toolBar);
-    QAction* addActOption = toolBar->addAction("+");
-    QAction* removeActOption = toolBar->addAction("-");
+    QAction* addActOption = toolBar->addAction("Add");
 
     connect(addActOption, &QAction::triggered, this, &NodeForm::newActOption);
-
-    connect(removeActOption, &QAction::triggered, [](){
-
-    });
 
     for (auto& actOption : data.actOptions) {
         ui->formLayout_2->addWidget(addPlaceForActOption(actOption));
